1aa331224b4a9de032d9e60215da56a0caad7cd0
Khang <dorayakihi@gmail.com>
2020-11-13 19:35:36 +0700

    cors

--- src/main/java/com/chatApp/sp/authservice/config/WebSecurityConfig.java
+++ src/main/java/com/chatApp/sp/authservice/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.chatApp.sp.authservice.config;
 
 
+import java.util.Arrays;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Bean;
@@ -93,17 +95,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	 @Bean
 	    public CorsConfigurationSource corsConfigurationSource() {
 	        final CorsConfiguration configuration = new CorsConfiguration();
-	        configuration.setAllowedOrigins(ImmutableList.of("*"));
-	        configuration.setAllowedMethods(ImmutableList.of("HEAD",
-	                "GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"));
+	        configuration.setAllowedOrigins(Arrays.asList("*","http://localhost:8080"));
+	        configuration.setAllowedMethods(Arrays.asList("HEAD","GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"));
 	        // setAllowCredentials(true) is important, otherwise:
 	        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
 	        configuration.setAllowCredentials(true);
 	        // setAllowedHeaders is important! Without it, OPTIONS preflight request
 	        // will fail with 403 Invalid CORS request
-	        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+	        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
 	        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 	        source.registerCorsConfiguration("/**", configuration);
+	        source.registerCorsConfiguration("/auth", configuration);
 	        return source;
 	    }
 
