0f832c7d76c61334edb3064af72bd508ee9e9275
Lê Duy Thanh <thanhlop859@gmail.com>
2020-12-13 02:33:14 +0700

    thanh

--- src/main/resources/static/js/sentImage.js
+++ src/main/resources/static/js/sentImage.js
@@ -1,28 +1,28 @@
 
-  $("image_upload_form").addEventListener('submit', (event) =>{
-    event.preventDefault();
+//   $("image_upload_form").addEventListener('submit', (event) =>{
+//     event.preventDefault();
   
-    var data = new FormData(this);
-    data.append("name", chatting);
+//     var data = new FormData(this);
+//     data.append("name", chatting);
+//     console.log(data);
+//     $.ajax({
+//           type: "POST",
+//           enctype: 'multipart/form-data',
+//           url: "/messages/img",
+//           data: data,
   
-    $.ajax({
-          type: "POST",
-          enctype: 'multipart/form-data',
-          url: "/messages/img",
-          data: data,
+//           // prevent jQuery from automatically transforming the data into a query string
+//           processData: false,
+//           contentType: false,
+//           cache: false,
+//           timeout: 1000000,
+//           success: function(data, textStatus, jqXHR) {
   
-          // prevent jQuery from automatically transforming the data into a query string
-          processData: false,
-          contentType: false,
-          cache: false,
-          timeout: 1000000,
-          success: function(data, textStatus, jqXHR) {
+//               console.log(data);
+//           },
+//           error: function(jqXHR, textStatus, errorThrown) {
+//             console.log("lỗi rồi bà con ơi!!");
   
-              console.log(data);
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-            console.log("lỗi rồi bà con ơi!!");
-  
-          }
-      });
-  });
+//           }
+//       });
+//   });
