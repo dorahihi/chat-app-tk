159ca36149f2f080efb06657f8fcc5b13a8197d2
Lê Duy Thanh <thanhlop859@gmail.com>
2020-12-13 22:07:07 +0700

    thah

--- src/main/resources/static/js/sentImage.js
+++ src/main/resources/static/js/sentImage.js
@@ -4,18 +4,18 @@ let upload = document.getElementById('image_upload_form');
   
     let img = new FormData(upload);
     let chatId = getChatId(chatting);
-    let data = {};
-    data.append('recipient',chatting);
-    data.append('sender',user.email);
-    data.append('type',chatId.type);
-    data.append('image',img);
-    data.append('mesType','Image');
-    data.append('chatId',chatId.id);
+    img.append('recipient',chatting);
+    img.append('sender',user.email);
+    img.append('type',chatId.type);
+    img.append('image',img);
+    img.append('mesType','Image');
+    img.append('chatId',chatId.id);
+    console.log(img);
     $.ajax({
           type: "POST",
           enctype: 'multipart/form-data',
           url: "/messages/img",
-          data: data,
+          data: img,
           // prevent jQuery from automatically transforming the data into a query string
           processData: false,
           contentType: false,
